76cbb80c9d560fbf7e04743d19f5b1c20bb89310
Sean Kelly <sevendaystillsean@gmail.com>
2023-10-16 22:56:34 -0700

    Added timestamps

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import asyncio
 from wyzeapy import Wyzeapy
 import aiomqtt
 import json
+import datetime
 
 TOPIC_STATE_REQUESTED = "wyze-python/floodlight/requested_state"
 TOPIC_STATE_ACTUAL = "wyze-python/floodlight/actual_state"
@@ -11,22 +12,29 @@ from dotenv import dotenv_values
 secrets = dotenv_values(".env")
 
 
+def log(message):
+    print(
+        datetime.datetime.strftime(datetime.datetime.now(), "%m/%d/%Y %H:%M:%S"),
+        message,
+    )
+
+
 async def mqtt_listen(camera_service, floodlight_cam):
     async with aiomqtt.Client(secrets["MQTT_HOST"]) as client:
         async with client.messages() as messages:
             await client.subscribe(TOPIC_STATE_REQUESTED)
             async for message in messages:
                 message.payload = message.payload.decode("utf-8")
-                print("New Received message ", message.topic, message.payload)
+                log("New Received message ", message.topic, message.payload)
                 if message.topic.matches(TOPIC_STATE_REQUESTED):
                     if message.payload == "true":
-                        print("TURNING ON")
+                        log("TURNING ON")
                         await camera_service.floodlight_on(floodlight_cam)
                     elif message.payload == "false":
-                        print("TURNING OFF")
+                        log("TURNING OFF")
                         await camera_service.floodlight_off(floodlight_cam)
                     else:
-                        print("Unknown command received: ", message.payload)
+                        log("Unknown command received: ", message.payload)
 
 
 async def async_main():
@@ -39,7 +47,7 @@ async def async_main():
                 api_key=secrets["WYZE_API_KEY"],
                 key_id=secrets["WYZE_API_KEY_ID"],
             )
-            print("Connected to Wyze API")
+            log("Connected to Wyze API")
 
             camera_service = await client.camera_service
             cameras = await camera_service.get_cameras()
@@ -47,11 +55,11 @@ async def async_main():
             floodlight_cam = next(
                 camera for camera in cameras if camera.nickname == "Floodlight"
             )
-            print("Got floodlight camera")
+            log("Got floodlight camera")
 
             tg.create_task(mqtt_listen(camera_service, floodlight_cam))
 
-            print("Starting main loop")
+            log("Starting main loop")
             while True:
                 floodlight_cam = await camera_service.update(floodlight_cam)
 
@@ -65,7 +73,7 @@ async def async_main():
                 }
                 state_json = json.dumps(state, indent=4)
 
-                # print(state_json)
+                # log(state_json)
 
                 await mqtt_client.publish(TOPIC_STATE_ACTUAL, payload=state_json)
                 await asyncio.sleep(1)
